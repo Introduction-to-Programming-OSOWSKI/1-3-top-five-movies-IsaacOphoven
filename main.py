8b6948461c9f8497b8d1c30970c56c2a49d34b66
isaacophoven <824067@mystma.org>
2021-09-21 09:08:58 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
 #WRITE YOUR CODE IN THIS FILE
+#ok
+
+def topFiveMovies(a,b,c,d,e):
+    return(a, b, c, d, e,)
+
+topFiveMovies("Spider-Man 2", "Pirates of the Caribean 3", "Pirates of the Caribean 2", "Pirates of the Caribean 1", "Captian Underpants")
